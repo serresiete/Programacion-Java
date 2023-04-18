448e5c53fea93cf1fe5a280bd47e292324171b80
Jorge Sierra <jorgesiegar@gmail.com>
2023-04-18 13:55:59 +0200

    Cambios "Hugi"

--- Pruebas/holamundo.java
+++ Pruebas/holamundo.java
@@ -3,5 +3,6 @@ package Pruebas;
 public class holamundo {
     public static void main(String[] args) {
         System.out.println("Hola Mundo");
+        System.out.println("Hola Hugi");
     }
 }
